93e985b3f609c21ac46332a8b8da28bd81cc1c0e
Joel Klasa <jklasa@purdue.edu>
2017-11-19 01:14:16 -0500

    INITIAL

---
+++ classify.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, redirect, g, render_template, url_for
+from flask_bootstrap import Bootstrap
+from spotify_api import *
+
+app = Flask(__name__)
+Bootstrap(app)
+
+# Server-side Parameters
+CLIENT_SIDE_URL = "http://127.0.0.1"
+PORT = 8080
+REDIRECT_URI = "{}:{}".format(CLIENT_SIDE_URL, PORT)
+
+
+@app.route("/")
+def login():
+    # Auth Step 1: Authorization using the login.html page
+    auth_url = get_auth_url(REDIRECT_URI + "/tokenrecv")
+    return render_template("login.html", login_url=auth_url)
+
+@app.route("/tokenrecv")
+def tokenrecv():
+    # Auth Step 4 and 5: Requests refresh and access tokens
+    auth_token = request.args['code']
+    response_data = get_access_tokens(auth_token, REDIRECT_URI + "/tokenrecv")
+    
+    if 'error' in response_data:
+        return redirect(url_for("error") + "?error={}&desc={}".format(response_data['error'], response_data['error_description']))
+
+    access_token = response_data["access_token"]
+    #refresh_token = response_data["refresh_token"]
+    #token_type = response_data["token_type"]
+    #expires_in = response_data["expires_in"]
+
+    return redirect(url_for("playlists") + "?token=" + access_token, 302)
+
+
+@app.route("/playlists")
+def playlists():
+    # Auth Step 6: Use the access token to access Spotify API
+    access_token = request.args['token']
+    authorization_header = {"Authorization":"Bearer {}".format(access_token)}
+
+    # Get profile data
+    profile_data = get_profile(authorization_header)
+    
+    # Get user playlist data
+    playlist_data = get_playlists(authorization_header)
+    
+    # Display playlist data
+    return render_template("playlists.html", playlists=playlist_data["items"])
+
+
+@app.route("/error")
+def error():
+    error = request.args['error']
+    desc = request.args['desc']
+
+    return render_template("error.html", error=error, description=desc)
+
+if __name__ == "__main__":
+    app.run(debug=True,port=PORT)
---
+++ spotify_api.py
@@ -0,0 +1,95 @@
+import base64
+import json
+import requests
+import urllib
+
+# Client keys
+CLIENT_ID = "688bd9390f694368ba3a5f523ecf7057"
+CLIENT_SECRET = "4ce83b9045884ee9b508a9bbdef738af"
+
+# Spotify Parameters
+SCOPE = "user-library-read playlist-read-private playlist-read-collaborative"
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+# Spotify URLs
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+SPOTIFY_API_VERSION = "v1"
+
+# Spotify Endpoints
+SPOTIFY_API_ENDPOINT = "{}/{}".format(SPOTIFY_API_BASE_URL, SPOTIFY_API_VERSION)
+GET_PROFILE_ENDPOINT = SPOTIFY_API_ENDPOINT + "/me"
+GET_PLAYLISTS_ENDPOINT = SPOTIFY_API_ENDPOINT + "/me/playlists"
+
+def get_auth_url(redirect_uri):
+    auth_query_parameters = {
+        "response_type": "code",
+        "redirect_uri": redirect_uri,
+        "scope": SCOPE,
+        # "state": STATE,
+        # "show_dialog": SHOW_DIALOG_str,
+        "client_id": CLIENT_ID
+    }
+
+    url_args = "&".join(["{}={}".format(key,urllib.quote(val)) for key,val in auth_query_parameters.iteritems()])
+    return "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+
+def get_access_tokens(auth_token, redirect_uri):
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": redirect_uri
+    }
+
+    b64enc = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_SECRET))
+    headers = {"Authorization": "basic {}".format(b64enc)}
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
+    return json.loads(post_request.text)
+
+def get_authorized(auth_header, url):
+    resp = requests.get(url, headers=auth_header)
+    return json.loads(resp.text)
+
+def get_unauthorized(url):
+    resp = requests.get(url)
+    return json.loads(resp.text)
+
+def get_profile(auth_header):
+    url = GET_PROFILE_ENDPOINT
+    return get_authorized(auth_header, url)
+
+def get_playlists(auth_header):
+    url = GET_PLAYLISTS_ENDPOINT
+    resp = requests.get(url, headers=auth_header)
+    return json.loads(resp.text)
+
+# https://developer.spotify.com/web-api/get-artist/
+def get_artist(artist_id):
+    url = GET_ARTIST_ENDPOINT.format(id=artist_id)
+    resp = requests.get(url)
+    return resp.json()
+
+
+# https://developer.spotify.com/web-api/search-item/
+def search_by_artist_name(name):
+    myparams = {'type': 'artist'}
+    myparams['q'] = name
+    resp = requests.get(SEARCH_ENDPOINT, params=myparams)
+    return resp.json()
+
+
+# https://developer.spotify.com/web-api/get-related-artists/
+def get_related_artists(artist_id):
+    url = RELATED_ARTISTS_ENDPOINT.format(id=artist_id)
+    resp = requests.get(url)
+    return resp.json()
+
+# https://developer.spotify.com/web-api/get-artists-top-tracks/
+def get_artist_top_tracks(artist_id, country='US'):
+    url = TOP_TRACKS_ENDPOINT.format(id=artist_id)
+    myparams = {'country': country}
+    resp = requests.get(url, params=myparams)
+    return resp.json()
